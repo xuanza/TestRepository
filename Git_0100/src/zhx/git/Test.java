0a476753f5a297e43499e48b9d7ada838ae2c91b
xuanza <wenfei123505@163.com>
2019-10-10 18:32:39 +0800

    branch1

--- Git_0100/src/zhx/git/Test.java
+++ Git_0100/src/zhx/git/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("git1");
+		System.out.println("new branch1");
+		System.out.println("new branch1 balabala");
 	}
 
 }
